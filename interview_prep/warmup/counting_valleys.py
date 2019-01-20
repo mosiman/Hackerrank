944c5c95e25de72486ce7f77ddcd1a75c3664618
mosiman <dillonchan97@gmail.com>
2019-01-20 11:13:50 -0500

    solution for counting valleys

---
+++ interview_prep/warmup/counting_valleys.py
@@ -0,0 +1,22 @@
+
+
+n = int(input())
+topo = input()
+
+currAltitude = 0
+numValleys = 0
+currentlyInValley = False
+
+for step in topo:
+    if step == "U":
+        currAltitude += 1
+    else
+        currAltitude -= 1
+
+    if currAltitude < 0 and not currentlyInValley:
+        currentlyInValley = True
+        numValleys += 1
+    if currAltitude >= 0 and currentlyInValley:
+        currentlyInValley = False
+
+print(numValleys)
