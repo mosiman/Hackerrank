6dfb3952f6472ac3b6b1e2e9300e55016bc4f6c8
mosiman <dillonchan97@gmail.com>
2019-01-20 12:05:42 -0500

    added rstrip to input, fixed edge case

--- interview_prep/warmup/jumping_on_clouds.py
+++ interview_prep/warmup/jumping_on_clouds.py
@@ -2,7 +2,7 @@
 
 n = int(input())
 
-clouds = list(map(int, input().split(' ')))
+clouds = list(map(int, input().rstrip().split(' ')))
 
 # Q: will greedy always minimize?
 # i.e., always jump 2. If blockage, jump 1.
@@ -11,11 +11,15 @@ clouds = list(map(int, input().split(' ')))
 currPos = 0
 numMoves = 0
 
-while currPos < n-1:
-    if clouds[currPos+2] != 1:
-        currPos += 2
-    else:
+while currPos <= n-2:
+    try:
+        if clouds[currPos+2] != 1:
+            currPos += 2
+        else:
+            currPos += 1
+    except:
         currPos += 1
+
     numMoves += 1
 
 print(numMoves)
